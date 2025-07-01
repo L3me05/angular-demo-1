f16ac97a923f751e2688d9348170fc41f525bacd
L3me05 <lemueldimarcantonio.elite@gmail.com>
2025-07-01 16:02:51 +0200

    feat: added alert component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,15 @@ import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
 import {Timeline} from './shared/components/timeline';
 import {AccordionItem} from './shared/components/accordion-item';
+import {Alert} from './shared/components/alert';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, Timeline, AccordionItem],
+  imports: [CommonModule, Timeline, AccordionItem, Alert],
   template: `
+<!--        timeline-->
     <div class="flex flex-col p-8 gap-4">
 
       <button class="btn" (click)="vertical=!vertical">Change</button>
@@ -16,6 +18,7 @@ import {AccordionItem} from './shared/components/accordion-item';
       <app-timeline [items]="timeline" [vertical]="vertical"/>
     </div>
 
+<!--        accordion item-->
     <div class="">
       <app-accordion-item title="one" selected>
         lorem ipsum
@@ -31,7 +34,17 @@ import {AccordionItem} from './shared/components/accordion-item';
 
     </div>
 
-
+<!--        Alert-->
+    <div class="flex flex-col p-6 gap-6">
+      <app-alert
+        (onCancel)="cancel()"
+        (onConfirm)="approve()"
+      />
+      <app-alert
+        (onCancel)="cancel2()"
+        (onConfirm)="approve2()"
+      />
+    </div>
 
 
   `,
@@ -46,19 +59,35 @@ import {AccordionItem} from './shared/components/accordion-item';
 
 
 export class App {
-timeline: {start: string, end: string}[] = [
-  { start: '2014', end: 'description' },
-  { start: '2015', end: 'lorem...' },
-  { start: '2018', end: 'bla bla' },
-  { start: '2022', end: 'another' },
-  { start: '2023', end: 'hello' },
-];
+  timeline: {start: string, end: string}[] = [
+    { start: '2014', end: 'description' },
+    { start: '2015', end: 'lorem...' },
+    { start: '2018', end: 'bla bla' },
+    { start: '2022', end: 'another' },
+    { start: '2023', end: 'hello' },
+  ];
 
-vertical= false;
+  vertical= false;
 
-doSomething() {
-  window.alert('hello');
-}
+  doSomething() {
+    window.alert('hello');
+  }
+
+  approve() {
+    window.alert('approved');
+  }
+
+  cancel() {
+    window.alert('canceled');
+  }
+
+  approve2(){
+    console.log("you are approved")
+  }
+
+  cancel2() {
+    console.log("you are canceled")
+  }
 
 
 
---
+++ src/app/shared/components/alert.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  imports: [],
+  template: `
+    <div role="alert" class="alert alert-vertical sm:alert-horizontal">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info h-6 w-6 shrink-0">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+      </svg>
+      <span>Hola bro</span>
+      <div>
+        <button class="btn btn-sm" (click)="onCancel.emit()">Deny</button>
+        <button class="btn btn-sm btn-primary" (click)="onConfirm.emit()">Accept</button>
+      </div>
+    </div>
+  `,
+  styles: ``
+})
+export class Alert {
+  @Output() onCancel = new EventEmitter()
+  @Output() onConfirm = new EventEmitter()
+
+}
