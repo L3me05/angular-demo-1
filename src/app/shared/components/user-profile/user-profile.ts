b81bea6eb62a1399271c60deea525db8ea9e7879
L3me05 <lemueldimarcantonio.elite@gmail.com>
2025-07-01 11:08:37 +0200

    Fine capitolo

--- src/app/shared/components/user-profile/user-profile.ts
+++ src/app/shared/components/user-profile/user-profile.ts
@@ -1,37 +1,53 @@
-import {Component, inject, Input, OnChanges, SimpleChange, SimpleChanges} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, Input} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../../../model/user';
 import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-user-profile',
+  changeDetection: ChangeDetectionStrategy.OnPush,        //migliora le performace da prestare attenzione.
   imports: [
     JsonPipe
   ],
   template: `
     <p>
-      Current id: {{ id }}
+      Current id: {{ userId }}
     </p>
 
     <pre>
       {{user | json}}
     </pre>
+
+    {{render()}}
   `,
   styles: ``
 })
-export class UserProfile implements OnChanges{
-  @Input() id: number | undefined;
+export class UserProfile {
+  userId: number | undefined;
+  @Input() set id (val: number | undefined){
+    this.userId = val;
+    this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${val}`)
+      .subscribe(res => {
+        this.user = res;
+      })
+  }
   http = inject(HttpClient)
   user: User | undefined;
 
-  ngOnChanges ( changes:SimpleChanges) {
-    console.log('ngOnChanges', changes)
 
-    this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${changes['id'].currentValue}`)
-      .subscribe(res => {
-        this.user =res;
-      })
+  render() {
+    console.log('render user-profile');
   }
+
+  //
+  // ngOnChanges ( changes:SimpleChanges) {
+  //   console.log('ngOnChanges', changes)
+  //
+  //   this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${changes['id'].currentValue}`)
+  //     .subscribe(res => {
+  //       this.user =res;
+  //     })
+  // }
 
 
 }
